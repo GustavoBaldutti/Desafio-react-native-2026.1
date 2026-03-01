aae1daaa1985b7eb12d53b9e1221ec9f31b10757
Gustavo Baldutti <baldutti.gustavo@estudante.ufjf.br>
2026-03-01 18:42:17 -0300

    refactor: ajustes no data

---
+++ src/data/data.ts
@@ -0,0 +1,118 @@
+export const skates = [
+  {
+    id: 1,
+    imagem: require("../assets/images/skateDgk.png"),
+    titulo: "Skate Street",
+    price: "R$ 399,99",
+  },
+
+  {
+    id: 2,
+    imagem: require("../assets/images/Skaterosa.png"),
+    titulo: "Skate Street",
+    price: "R$ 399,99",
+  },
+
+  {
+    id: 3,
+    imagem: require("../assets/images/skateDgk.png"),
+    titulo: "Skate Street",
+    price: "R$ 399,99",
+  },
+
+  {
+    id: 4,
+    imagem: require("../assets/images/Skaterosa.png"),
+    titulo: "Skate Street",
+    price: "R$ 399,99",
+  },
+];
+
+export const rodinhas = [
+  {
+    id: 5,
+    imagem: require("../assets/images/rodinhaPreta.png"),
+    titulo: "rodinhas",
+    price: "R$ 199,99",
+  },
+
+  {
+    id: 6,
+    imagem: require("../assets/images/rodinhaPreta.png"),
+    titulo: "rodinhas",
+    price: "R$ 199,99",
+  },
+
+  {
+    id: 7,
+    imagem: require("../assets/images/rodinhaPreta.png"),
+    titulo: "Rodinhas",
+    price: "R$ 199,99",
+  },
+
+  {
+    id: 8,
+    imagem: require("../assets/images/rodinhaPreta.png"),
+    titulo: "Rodinhas",
+    price: "R$ 199,99",
+  },
+];
+
+export const shapes = [
+  {
+    id: 9,
+    imagem: require("../assets/images/shapePorco.png"),
+    titulo: "Shapes",
+    price: "R$ 299,99",
+  },
+
+  {
+    id: 10,
+    imagem: require("../assets/images/shapeCav.png"),
+    titulo: "Shapes",
+    price: "R$ 299,99",
+  },
+
+  {
+    id: 11,
+    imagem: require("../assets/images/shapePorco.png"),
+    titulo: "Shapes",
+    price: "R$ 299,99",
+  },
+
+  {
+    id: 12,
+    imagem: require("../assets/images/shapeCav.png"),
+    titulo: "Shapes",
+    price: "R$ 299,99",
+  },
+];
+export const trucks = [
+  {
+    id: 13,
+    imagem: require("../assets/images/truck1.png"),
+    titulo: "Trucks",
+    price: "R$ 229,99",
+  },
+
+  {
+    id: 14,
+    imagem: require("../assets/images/truck2.png"),
+    titulo: "Trucks",
+    price: "R$ 229,99",
+  },
+
+  {
+    id: 15,
+    imagem: require("../assets/images/truck1.png"),
+    titulo: "Trucks",
+    price: "R$ 229,99",
+  },
+
+  {
+    id: 16,
+    imagem: require("../assets/images/truck2.png"),
+    titulo: "Trucks",
+    price: "R$ 229,99",
+  },
+];
