1d548e11f120cc5c70b952b1714dcf34318cf02a
Gustavo Baldutti <baldutti.gustavo@estudante.ufjf.br>
2026-03-01 18:37:44 -0300

    feat: criando footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000000',
+    width: '100%',
+    paddingVertical: 30, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 28,
+    fontWeight: '800',
+    textTransform: 'uppercase',
+    letterSpacing: 2, 
+  },
+});
